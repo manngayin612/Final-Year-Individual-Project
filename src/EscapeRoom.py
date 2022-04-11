cf32d32d639bac88aae34c8f992c0fc937f908c1
manngayin612 <manngayin@gmail.com>
2022-04-11 17:45:12 +0100

    Implemented identify object function

--- src/EscapeRoom.py
+++ src/EscapeRoom.py
@@ -21,90 +21,54 @@ def initialiseRoom():
     data = json.load(f)
     f.close()
 
-
-    # Set up items in the room
-    door = Item("door", actions=["unlock", "open"], status=["locked", "unlocked"], definition=["unlock.v.01",'open.v.01'])
-    # door.setSpecialTool("unlock", "key")
-    key = Item("key", definition=["get.v.01"], actions=["get"])
-    table = Item("table")
+    door = Item("door", item_def="door.n.01",actions=["unlock", "open"], status=["locked", "unlocked"], action_def=["unlock.v.01",'open.v.01'])
+    key = Item("key", item_def="key.n.01",action_def=["get.v.01"], actions=["get"])
+    table = Item("table", item_def="table.n.02", description="There is a key on the table.")
     objects.extend([door, key, table])
     print("Room Created!")
 
 
-# See if the item exists in the room
-def findItems(item):
+# Return the Item object with the
+def getItem(item_name):
     for obj in objects:
-        if item == obj.getName():
+        if item_name == obj.getName():
             return obj
-    return "No such item in the room"
+    return None
 
-# def processAction(action, subject, direct_object, indirect_object):
 
-#     item = findItems(direct_object)
 
-#     for a in item.getActions():
-#         print(a)
-#         if action in vr.getSynsetsList(a):
-#             if a == "pick_up":
-#                 bag.append(item.getName())
-#                 current_room_items.remove(item.getName())
-#             elif a == "unlock":
-#                 if item.getName() == "door":
-#                     if objects[1].getName() in bag:
-#                         item.setCurrentStatus("unlocked")
-#                     else:
-#                         print("The door is locked")
+def identifyObject(item):
+    max_similarity = 0
+    matching_obj = ""
+    for obj in objects:
+        for ss in wn.synsets(item, pos=wn.NOUN):
+            if(ss.name().split(".")[0] == item):
+                current_similarity = ss.lch_similarity(wn.synset(obj.getItemDef()))
+                if current_similarity > max_similarity:
+                    max_similarity = current_similarity
+                    matching_obj = obj
+    return matching_obj
 
-#             elif a == "investigate":
-#                 print(item.getDescription())
 
-        
-        
-#     print(current_room_items, bag)
-
-# Takes in a list of tuples (action, direct object) and carry out the action
-# def processAction(actions_dobjects):
-    
-#     for (action, direct_object) in actions_dobjects:
-
-#         item = findItems(direct_object)
-
-#         for (a,d) in item.getActions():
-#             print(a,d)
-#             if a in vr.getSynsetsList(action):
-  
-#                 if a == "get": # only append to bag when picking up things
-#                     bag.append(item.getName())
-#                     current_room_items.remove(item.getName())
-#                 elif a == "unlock":
-#                     if item.getName() == "door": # update status of the door when unlocking it
-#                         if objects[1].getName() in bag:
-#                             item.setCurrentStatus("unlocked")
-#                         else:
-#                             print("The door is locked")
-
-#                 elif a == "investigate": # return description of the item when investigate
-#                     print(item.getDescription())
-
-#         print(current_room_items, bag)
-
+def identifyAction(action, item):
+    max_similarity = 0
+    matching_action = ""
+    for (a,d) in item.getActions():
+        for ss in wn.synsets(action, pos=wn.VERB):
+            if(ss.name().split('.')[0] == action):
+                current_similarity = ss.lch_similarity(wn.synset(d)) 
+                if current_similarity > max_similarity:
+                    max_similarity = current_similarity
+                    matching_action = a
+    return matching_action if max_similarity > threshold else ""
 
 def processAction(actions_dobjects):
     
     for (action, direct_object) in actions_dobjects:
-        max_similarity = 0
-        matching_action = ""
-        item = findItems(direct_object) 
+        item = identifyObject(direct_object)
 
-        for (a,d) in item.getActions():
-            print(a,d)
-            for ss in wn.synsets(action, pos=wn.VERB):
-                if(ss.name().split('.')[0] == action):
-                    current_similarity = ss.lch_similarity(wn.synset(d)) 
-                    if current_similarity > max_similarity:
-                        max_similarity = current_similarity
-                        matching_action = a
-        if max_similarity > threshold:
+        matching_action = identifyAction(action, item)
+        if matching_action!="":
             if matching_action == "get":
                 bag.append(item.getName())
                 current_room_items.remove(item.getName())
@@ -116,27 +80,6 @@ def processAction(actions_dobjects):
                         print("The door is locked")
             elif matching_action == "investigate":
                 print(item.getDescription())
-
-
-
-
-
-
-
-            # if a in vr.getSynsetsList(action):
-  
-            #     if a == "get": # only append to bag when picking up things
-            #         bag.append(item.getName())
-            #         current_room_items.remove(item.getName())
-            #     elif a == "unlock":
-            #         if item.getName() == "door": # update status of the door when unlocking it
-            #             if objects[1].getName() in bag:
-            #                 item.setCurrentStatus("unlocked")
-            #             else:
-            #                 print("The door is locked")
-
-            #     elif a == "investigate": # return description of the item when investigate
-            #         print(item.getDescription())
 
         print(current_room_items, bag)
 
@@ -170,13 +113,9 @@ if __name__ == "__main__":
 
         print("end of game")
     else:
-        for ss in wn.synsets("investigate", pos= wn.VERB):
+        for ss in wn.synsets("table", pos= wn.NOUN):
             print(ss,ss.definition())
-        
-        # print(set([lemma for s in wn.synsets("get", pos = wn.VERB) for lemma in s.lemma_names()]))
-        # print(wn.synset("get.v.01").path_similarity(wn.synset("contract.v.04")))
-        # print(wn.synset("get.v.01").path_similarity(wn.synset("grab.v.05")))
-        # print(wn.synset("get.v.01").path_similarity(wn.synset("take.v.04")))
+
         
 
             
--- src/Item.py
+++ src/Item.py
@@ -3,17 +3,21 @@ from telnetlib import STATUS
 
 class Item:
 
-    def __init__(self, name, actions=["investigate"], status=[], description="blablabla", definition=["investigate.v.02"]):
+    def __init__(self, name, item_def, actions=["investigate"], action_def=["investigate.v.02"],status=[], description="blablabla"):
         self.__name = name
+        self.__item_def = item_def
         self.__actions = {"investigate":"investigate.v.02"}
         for i in range(len(actions)):
-            self.__actions[actions[i]] = definition[i]
+            self.__actions[actions[i]] = action_def[i]
         self.__status = status
         self.__currentStatus = "" if status == [] else status[0]
         self.__description = description
-        self.__def = definition
+        self.__def = action_def
         
+    def getItemDef(self):
+        return self.__item_def
 
+        
     def getActions(self):
         return self.__actions.items()
 
@@ -27,8 +31,8 @@ class Item:
         if (status in self.__status):
             self.__currentStatus = status
     
-    def getDescription(self, action):
-        return self.__actions[action]
+    def getDescription(self):
+        return self.__description
     
 
 
